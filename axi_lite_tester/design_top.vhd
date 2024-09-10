9c2bac60fca00babcebd4de384bbb96f6878b229
rodrigo olmos canovas <rodrigo.olmos.canovas@gmail.com>
2024-09-10 20:40:01 +0200

    mejorado claridad en el codigo

--- axi_lite_tester/design_top.vhd
+++ axi_lite_tester/design_top.vhd
@@ -21,21 +21,23 @@ entity design_top is
 end entity design_top;
 
 architecture rtl of design_top is
+    constant AXI_SLAVE_ADDR_WITH : natural := 4;
+    
     component axi_lite_top is
         port (
             
-            s00_axi_awaddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
-            s00_axi_awprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+            s00_axi_awaddr : in STD_LOGIC_VECTOR (AXI_SLAVE_ADDR_WITH - 1 downto 0 );
+            s00_axi_awprot : in STD_LOGIC_VECTOR (2 downto 0 );
             s00_axi_awvalid : in STD_LOGIC;
             s00_axi_awready : out STD_LOGIC;
-            s00_axi_wdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-            s00_axi_wstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+            s00_axi_wdata : in STD_LOGIC_VECTOR (31 downto 0 );
+            s00_axi_wstrb : in STD_LOGIC_VECTOR (3 downto 0 );
             s00_axi_wvalid : in STD_LOGIC;
             s00_axi_wready : out STD_LOGIC;
-            s00_axi_bresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+            s00_axi_bresp : out STD_LOGIC_VECTOR (1 downto 0 );
             s00_axi_bvalid : out STD_LOGIC;
             s00_axi_bready : in STD_LOGIC;
-            s00_axi_araddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+            s00_axi_araddr : in STD_LOGIC_VECTOR (AXI_SLAVE_ADDR_WITH - 1 downto 0 );
             s00_axi_arprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
             s00_axi_arvalid : in STD_LOGIC;
             s00_axi_arready : out STD_LOGIC;
@@ -47,7 +49,7 @@ architecture rtl of design_top is
             s00_axi_aresetn : in STD_LOGIC
         );
     end component axi_lite_top;
-    
+
     component master_axi_base_top is
         port (
               ------------- debug from tb ----------
@@ -106,7 +108,7 @@ architecture rtl of design_top is
     signal master_test_0_M00_AXI_WSTRB : STD_LOGIC_VECTOR ( 3 downto 0 );
     signal master_test_0_M00_AXI_WVALID : STD_LOGIC;
     signal master_test_0_m00_axi_error : STD_LOGIC;
-    
+
 begin
     
     master_test_0: component master_axi_base_top
@@ -124,50 +126,50 @@ begin
         -------------------------------------
         
         m00_axi_aclk => clk,
-        m00_axi_araddr(31 downto 0) => master_test_0_M00_AXI_ARADDR(31 downto 0),
+        m00_axi_araddr => master_test_0_M00_AXI_ARADDR(31 downto 0),
         m00_axi_aresetn => nrst,
-        m00_axi_arprot(2 downto 0) => master_test_0_M00_AXI_ARPROT(2 downto 0),
+        m00_axi_arprot => master_test_0_M00_AXI_ARPROT(2 downto 0),
         m00_axi_arready => master_test_0_M00_AXI_ARREADY,
         m00_axi_arvalid => master_test_0_M00_AXI_ARVALID,
-        m00_axi_awaddr(31 downto 0) => master_test_0_M00_AXI_AWADDR(31 downto 0),
-        m00_axi_awprot(2 downto 0) => master_test_0_M00_AXI_AWPROT(2 downto 0),
+        m00_axi_awaddr => master_test_0_M00_AXI_AWADDR(31 downto 0),
+        m00_axi_awprot => master_test_0_M00_AXI_AWPROT(2 downto 0),
         m00_axi_awready => master_test_0_M00_AXI_AWREADY,
         m00_axi_awvalid => master_test_0_M00_AXI_AWVALID,
         m00_axi_bready => master_test_0_M00_AXI_BREADY,
-        m00_axi_bresp(1 downto 0) => master_test_0_M00_AXI_BRESP(1 downto 0),
+        m00_axi_bresp => master_test_0_M00_AXI_BRESP(1 downto 0),
         m00_axi_bvalid => master_test_0_M00_AXI_BVALID,
-        m00_axi_rdata(31 downto 0) => master_test_0_M00_AXI_RDATA(31 downto 0),
+        m00_axi_rdata => master_test_0_M00_AXI_RDATA(31 downto 0),
         m00_axi_rready => master_test_0_M00_AXI_RREADY,
-        m00_axi_rresp(1 downto 0) => master_test_0_M00_AXI_RRESP(1 downto 0),
+        m00_axi_rresp => master_test_0_M00_AXI_RRESP(1 downto 0),
         m00_axi_rvalid => master_test_0_M00_AXI_RVALID,
-        m00_axi_wdata(31 downto 0) => master_test_0_M00_AXI_WDATA(31 downto 0),
+        m00_axi_wdata => master_test_0_M00_AXI_WDATA(31 downto 0),
         m00_axi_wready => master_test_0_M00_AXI_WREADY,
-        m00_axi_wstrb(3 downto 0) => master_test_0_M00_AXI_WSTRB(3 downto 0),
+        m00_axi_wstrb => master_test_0_M00_AXI_WSTRB(3 downto 0),
         m00_axi_wvalid => master_test_0_M00_AXI_WVALID
     );
     
     slave_lite_tb_0: component axi_lite_top
     port map (
         s00_axi_aclk => clk,
-        s00_axi_araddr(3 downto 0) => master_test_0_M00_AXI_ARADDR(3 downto 0),
+        s00_axi_araddr => master_test_0_M00_AXI_ARADDR(AXI_SLAVE_ADDR_WITH - 1 downto 0),
         s00_axi_aresetn => nrst,
-        s00_axi_arprot(2 downto 0) => master_test_0_M00_AXI_ARPROT(2 downto 0),
+        s00_axi_arprot => master_test_0_M00_AXI_ARPROT(2 downto 0),
         s00_axi_arready => master_test_0_M00_AXI_ARREADY,
         s00_axi_arvalid => master_test_0_M00_AXI_ARVALID,
-        s00_axi_awaddr(3 downto 0) => master_test_0_M00_AXI_AWADDR(3 downto 0),
-        s00_axi_awprot(2 downto 0) => master_test_0_M00_AXI_AWPROT(2 downto 0),
+        s00_axi_awaddr => master_test_0_M00_AXI_AWADDR(AXI_SLAVE_ADDR_WITH - 1 downto 0),
+        s00_axi_awprot => master_test_0_M00_AXI_AWPROT(2 downto 0),
         s00_axi_awready => master_test_0_M00_AXI_AWREADY,
         s00_axi_awvalid => master_test_0_M00_AXI_AWVALID,
         s00_axi_bready => master_test_0_M00_AXI_BREADY,
-        s00_axi_bresp(1 downto 0) => master_test_0_M00_AXI_BRESP(1 downto 0),
+        s00_axi_bresp => master_test_0_M00_AXI_BRESP(1 downto 0),
         s00_axi_bvalid => master_test_0_M00_AXI_BVALID,
-        s00_axi_rdata(31 downto 0) => master_test_0_M00_AXI_RDATA(31 downto 0),
+        s00_axi_rdata => master_test_0_M00_AXI_RDATA(31 downto 0),
         s00_axi_rready => master_test_0_M00_AXI_RREADY,
-        s00_axi_rresp(1 downto 0) => master_test_0_M00_AXI_RRESP(1 downto 0),
+        s00_axi_rresp => master_test_0_M00_AXI_RRESP(1 downto 0),
         s00_axi_rvalid => master_test_0_M00_AXI_RVALID,
-        s00_axi_wdata(31 downto 0) => master_test_0_M00_AXI_WDATA(31 downto 0),
+        s00_axi_wdata => master_test_0_M00_AXI_WDATA(31 downto 0),
         s00_axi_wready => master_test_0_M00_AXI_WREADY,
-        s00_axi_wstrb(3 downto 0) => master_test_0_M00_AXI_WSTRB(3 downto 0),
+        s00_axi_wstrb => master_test_0_M00_AXI_WSTRB(3 downto 0),
         s00_axi_wvalid => master_test_0_M00_AXI_WVALID
     );
 
