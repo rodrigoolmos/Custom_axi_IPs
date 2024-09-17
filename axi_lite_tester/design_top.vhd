e1a700d4531fd3b45fe04c671f6923943aa43e2a
rodrigo olmos <tic.rodrigo.olmos@gmail.com>
2024-09-17 11:02:30 +0200

    axi_lite_tester -> code cleaned and functions added to tb

--- axi_lite_tester/design_top.vhd
+++ axi_lite_tester/design_top.vhd
@@ -23,32 +23,32 @@ end entity design_top;
 architecture rtl of design_top is
     constant AXI_SLAVE_ADDR_WITH : natural := 4;
     
-    component axi_lite_top is
+    component axi_lite is
         port (
             
-            s00_axi_awaddr : in STD_LOGIC_VECTOR (AXI_SLAVE_ADDR_WITH - 1 downto 0 );
-            s00_axi_awprot : in STD_LOGIC_VECTOR (2 downto 0 );
-            s00_axi_awvalid : in STD_LOGIC;
-            s00_axi_awready : out STD_LOGIC;
-            s00_axi_wdata : in STD_LOGIC_VECTOR (31 downto 0 );
-            s00_axi_wstrb : in STD_LOGIC_VECTOR (3 downto 0 );
-            s00_axi_wvalid : in STD_LOGIC;
-            s00_axi_wready : out STD_LOGIC;
-            s00_axi_bresp : out STD_LOGIC_VECTOR (1 downto 0 );
-            s00_axi_bvalid : out STD_LOGIC;
-            s00_axi_bready : in STD_LOGIC;
-            s00_axi_araddr : in STD_LOGIC_VECTOR (AXI_SLAVE_ADDR_WITH - 1 downto 0 );
-            s00_axi_arprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
-            s00_axi_arvalid : in STD_LOGIC;
-            s00_axi_arready : out STD_LOGIC;
-            s00_axi_rdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
-            s00_axi_rresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
-            s00_axi_rvalid : out STD_LOGIC;
-            s00_axi_rready : in STD_LOGIC;
-            s00_axi_aclk : in STD_LOGIC;
-            s00_axi_aresetn : in STD_LOGIC
+            S_AXI_ACLK    : in std_logic;
+            S_AXI_ARESETN    : in std_logic;
+            S_AXI_AWADDR    : in std_logic_vector(AXI_SLAVE_ADDR_WITH - 1 downto 0);
+            S_AXI_AWPROT    : in std_logic_vector(2 downto 0);
+            S_AXI_AWVALID    : in std_logic;
+            S_AXI_AWREADY    : out std_logic;
+            S_AXI_WDATA    : in std_logic_vector(31 downto 0);
+            S_AXI_WSTRB    : in std_logic_vector(3 downto 0);
+            S_AXI_WVALID    : in std_logic;
+            S_AXI_WREADY    : out std_logic;
+            S_AXI_BRESP    : out std_logic_vector(1 downto 0);
+            S_AXI_BVALID    : out std_logic;
+            S_AXI_BREADY    : in std_logic;
+            S_AXI_ARADDR    : in std_logic_vector(AXI_SLAVE_ADDR_WITH - 1 downto 0);
+            S_AXI_ARPROT    : in std_logic_vector(2 downto 0);
+            S_AXI_ARVALID    : in std_logic;
+            S_AXI_ARREADY    : out std_logic;
+            S_AXI_RDATA    : out std_logic_vector(31 downto 0);
+            S_AXI_RRESP    : out std_logic_vector(1 downto 0);
+            S_AXI_RVALID    : out std_logic;
+            S_AXI_RREADY    : in std_logic
         );
-    end component axi_lite_top;
+    end component axi_lite;
 
     component master_axi_base_top is
         port (
@@ -148,29 +148,29 @@ begin
         m00_axi_wvalid => master_test_0_M00_AXI_WVALID
     );
     
-    slave_lite_tb_0: component axi_lite_top
+    slave_lite_tb_0: component axi_lite
     port map (
-        s00_axi_aclk => clk,
-        s00_axi_araddr => master_test_0_M00_AXI_ARADDR(AXI_SLAVE_ADDR_WITH - 1 downto 0),
-        s00_axi_aresetn => nrst,
-        s00_axi_arprot => master_test_0_M00_AXI_ARPROT(2 downto 0),
-        s00_axi_arready => master_test_0_M00_AXI_ARREADY,
-        s00_axi_arvalid => master_test_0_M00_AXI_ARVALID,
-        s00_axi_awaddr => master_test_0_M00_AXI_AWADDR(AXI_SLAVE_ADDR_WITH - 1 downto 0),
-        s00_axi_awprot => master_test_0_M00_AXI_AWPROT(2 downto 0),
-        s00_axi_awready => master_test_0_M00_AXI_AWREADY,
-        s00_axi_awvalid => master_test_0_M00_AXI_AWVALID,
-        s00_axi_bready => master_test_0_M00_AXI_BREADY,
-        s00_axi_bresp => master_test_0_M00_AXI_BRESP(1 downto 0),
-        s00_axi_bvalid => master_test_0_M00_AXI_BVALID,
-        s00_axi_rdata => master_test_0_M00_AXI_RDATA(31 downto 0),
-        s00_axi_rready => master_test_0_M00_AXI_RREADY,
-        s00_axi_rresp => master_test_0_M00_AXI_RRESP(1 downto 0),
-        s00_axi_rvalid => master_test_0_M00_AXI_RVALID,
-        s00_axi_wdata => master_test_0_M00_AXI_WDATA(31 downto 0),
-        s00_axi_wready => master_test_0_M00_AXI_WREADY,
-        s00_axi_wstrb => master_test_0_M00_AXI_WSTRB(3 downto 0),
-        s00_axi_wvalid => master_test_0_M00_AXI_WVALID
+        S_AXI_ACLK => clk,
+        S_AXI_ARADDR => master_test_0_M00_AXI_ARADDR(AXI_SLAVE_ADDR_WITH - 1 downto 0),
+        S_AXI_ARESETN => nrst,
+        S_AXI_ARPROT => master_test_0_M00_AXI_ARPROT(2 downto 0),
+        S_AXI_ARREADY => master_test_0_M00_AXI_ARREADY,
+        S_AXI_ARVALID => master_test_0_M00_AXI_ARVALID,
+        S_AXI_AWADDR => master_test_0_M00_AXI_AWADDR(AXI_SLAVE_ADDR_WITH - 1 downto 0),
+        S_AXI_AWPROT => master_test_0_M00_AXI_AWPROT(2 downto 0),
+        S_AXI_AWREADY => master_test_0_M00_AXI_AWREADY,
+        S_AXI_AWVALID => master_test_0_M00_AXI_AWVALID,
+        S_AXI_BREADY => master_test_0_M00_AXI_BREADY,
+        S_AXI_BRESP => master_test_0_M00_AXI_BRESP(1 downto 0),
+        S_AXI_BVALID => master_test_0_M00_AXI_BVALID,
+        S_AXI_RDATA => master_test_0_M00_AXI_RDATA(31 downto 0),
+        S_AXI_RREADY => master_test_0_M00_AXI_RREADY,
+        S_AXI_RRESP => master_test_0_M00_AXI_RRESP(1 downto 0),
+        S_AXI_RVALID => master_test_0_M00_AXI_RVALID,
+        S_AXI_WDATA => master_test_0_M00_AXI_WDATA(31 downto 0),
+        S_AXI_WREADY => master_test_0_M00_AXI_WREADY,
+        S_AXI_WSTRB => master_test_0_M00_AXI_WSTRB(3 downto 0),
+        S_AXI_WVALID => master_test_0_M00_AXI_WVALID
     );
 
 end architecture rtl;
--- axi_lite_tester/design_top_tb.vhd
+++ axi_lite_tester/design_top_tb.vhd
@@ -23,19 +23,20 @@ architecture rtl of design_top_tb is
     signal test_araddr : std_logic_vector(31 downto 0);
     signal test_rdata : std_logic_vector(31 downto 0);
 
+    type RAM_TYPE is array (0 to 3) of std_logic_vector(31 downto 0);
+        signal ram_read : RAM_TYPE;
 
-    
     component design_top is
         port (
         ------------- debug from tb ----------
-        test_wdata  : in std_logic_vector(31 downto 0);
-        test_awaddr : in std_logic_vector(31 downto 0);
-        test_araddr : in std_logic_vector(31 downto 0);
-        test_rdata  : out std_logic_vector(31 downto 0);
-        m00_axi_init_axi_txn : in STD_LOGIC;
-        m00_axi_init_axi_rxn : in STD_LOGIC;
-        m00_axi_txn_ready : out STD_LOGIC;
-        m00_axi_rxn_ready : out STD_LOGIC;
+            test_wdata  : in std_logic_vector(31 downto 0);
+            test_awaddr : in std_logic_vector(31 downto 0);
+            test_araddr : in std_logic_vector(31 downto 0);
+            test_rdata  : out std_logic_vector(31 downto 0);
+            m00_axi_init_axi_txn : in STD_LOGIC;
+            m00_axi_init_axi_rxn : in STD_LOGIC;
+            m00_axi_txn_ready : out STD_LOGIC;
+            m00_axi_rxn_ready : out STD_LOGIC;
         -------------------------------------
             
             clk : in STD_LOGIC;
@@ -43,6 +44,48 @@ architecture rtl of design_top_tb is
 
         );
     end component design_top;
+
+    procedure send_data(
+        constant data                   : std_logic_vector ( 31 downto 0 );
+        constant addr                   : std_logic_vector ( 31 downto 0 );
+        signal m00_axi_init_axi_txn0    : out std_logic;
+        signal m00_axi_txn_ready0       : in  std_logic;
+        signal test_wdata0              : out std_logic_vector ( 31 downto 0 );
+        signal test_awaddr0             : out std_logic_vector ( 31 downto 0 )
+    ) is
+    begin
+
+        wait until rising_edge(clk);
+        m00_axi_init_axi_txn0 <= '1';
+        test_wdata0 <= data;
+        test_awaddr0 <= addr;
+        wait until rising_edge(clk);
+        m00_axi_init_axi_txn0 <= '0';
+        wait until m00_axi_txn_ready0 = '1';
+        wait until rising_edge(clk);
+
+    end procedure;
+
+    procedure recive_data(
+        signal data                     : out std_logic_vector ( 31 downto 0 );
+        constant addr                   : std_logic_vector ( 31 downto 0 );
+        signal m00_axi_init_axi_rxn0    : out std_logic;
+        signal m00_axi_rxn_ready0       : in  std_logic;
+        signal test_araddr0             : out std_logic_vector ( 31 downto 0 );
+        signal test_rdata0               : in std_logic_vector(31 downto 0)
+    ) is
+    begin
+
+        wait until rising_edge(clk);
+        m00_axi_init_axi_rxn0 <= '1';
+        test_araddr0 <= addr;
+        wait until rising_edge(clk);
+        m00_axi_init_axi_rxn0 <= '0';
+        wait until m00_axi_rxn_ready0 = '1';
+        data <= test_rdata;
+        wait until rising_edge(clk);
+
+    end procedure;
     
 begin
 
@@ -68,58 +111,18 @@ begin
         nrst <= '1';
         wait until rising_edge(clk);
 
-        m00_axi_init_axi_txn <= '1';
-        test_wdata <= x"12345678";
-        test_awaddr <= x"12345670";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_txn <= '0';
-        wait until m00_axi_txn_ready = '1';
+        send_data(x"12345678", x"12345670", m00_axi_init_axi_txn, m00_axi_txn_ready, test_wdata, test_awaddr);
+        send_data(x"11223344", x"12345674", m00_axi_init_axi_txn, m00_axi_txn_ready, test_wdata, test_awaddr);
+        send_data(x"BADC0FFE", x"12345678", m00_axi_init_axi_txn, m00_axi_txn_ready, test_wdata, test_awaddr);
+        send_data(x"DEADFEED", x"1234567C", m00_axi_init_axi_txn, m00_axi_txn_ready, test_wdata, test_awaddr);
 
-        m00_axi_init_axi_txn <= '1';
-        test_wdata <= x"11223344";
-        test_awaddr <= x"12345674";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_txn <= '0';
-        wait until m00_axi_txn_ready = '1';
+        
+        recive_data( ram_read(0), x"12345670", m00_axi_init_axi_rxn, m00_axi_rxn_ready, test_araddr, test_rdata);
+        recive_data( ram_read(1), x"12345674", m00_axi_init_axi_rxn, m00_axi_rxn_ready, test_araddr, test_rdata);
+        recive_data( ram_read(2), x"12345678", m00_axi_init_axi_rxn, m00_axi_rxn_ready, test_araddr, test_rdata);
+        recive_data( ram_read(3), x"1234567C", m00_axi_init_axi_rxn, m00_axi_rxn_ready, test_araddr, test_rdata);
 
-        m00_axi_init_axi_txn <= '1';
-        test_wdata <= x"BADC0FFE";
-        test_awaddr <= x"12345678";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_txn <= '0';
-        wait until m00_axi_txn_ready = '1';
-
-        m00_axi_init_axi_txn <= '1';
-        test_wdata <= x"DEADFEED";
-        test_awaddr <= x"1234567C";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_txn <= '0';
-        wait until m00_axi_txn_ready = '1';
-
-        m00_axi_init_axi_rxn <= '1';
-        test_araddr <= x"12345670";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_rxn <= '0';
-        wait until m00_axi_rxn_ready = '1';
-
-        m00_axi_init_axi_rxn <= '1';
-        test_araddr <= x"12345674";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_rxn <= '0';
-        wait until m00_axi_rxn_ready = '1';
-
-        m00_axi_init_axi_rxn <= '1';
-        test_araddr <= x"12345678";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_rxn <= '0';
-        wait until m00_axi_rxn_ready = '1';
-
-        m00_axi_init_axi_rxn <= '1';
-        test_araddr <= x"1234567C";
-        wait until rising_edge(clk);
-        m00_axi_init_axi_rxn <= '0';
-        wait until m00_axi_rxn_ready = '1';
-        wait for 10 ms;
+        wait for 10 us;
         finished <= '1';
         
         wait;
