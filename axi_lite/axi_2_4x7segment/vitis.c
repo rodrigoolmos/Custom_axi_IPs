5faac052196baed1c89df54d7dfefefffd369a02
rodrigo olmos canovas <rodrigo.olmos.canovas@gmail.com>
2024-09-22 11:55:36 +0200

    folders and names reorganiced and ni ips added

---
+++ axi_lite/axi_2_4x7segment/vitis.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "xil_io.h"
+#include "sleep.h"
+#include "xil_io.h"
+
+#define NUM0 0xC0  // 0b11000000  -> NOT A, B, C, D, E, F
+#define NUM1 0xF9  // 0b11111001  -> NOT B, C
+#define NUM2 0xA4  // 0b10100100  -> NOT A, B, D, E, G
+#define NUM3 0xB0  // 0b10110000  -> NOT A, B, C, D, G
+#define NUM4 0x99  // 0b10011001  -> NOT B, C, F, G
+#define NUM5 0x92  // 0b10010010  -> NOT A, C, D, F, G
+#define NUM6 0x82  // 0b10000010  -> NOT A, C, D, E, F, G
+#define NUM7 0xF8  // 0b11111000  -> NOT A, B, C
+#define NUM8 0x80  // 0b00000000  -> NOT A, B, C, D, E, F, G
+#define NUM9 0x98  // 0b01101111  -> NOT A, B, C, D, F, G
+
+int main(){
+
+	volatile uint32_t data;
+
+
+	while(1){
+		data = Xil_In32(0x80000004);
+		Xil_Out32(0x80000004, 0x01010101);
+		Xil_Out32(0x80000000, NUM0 << 24 | NUM1 << 16 | NUM2 << 8 | NUM3);
+		data = Xil_In32(0x80000000);
+		Xil_Out32(0x80000004, 0x00000000);
+		Xil_Out32(0x80000000, 0x01020304);
+		Xil_Out32(0x80000000, 0x05060708);
+		Xil_Out32(0x80000000, 0x090A0B0C);
+		Xil_Out32(0x80000000, 0x0D0E0F00);
+		data = Xil_In32(0x80000000);
+		data = Xil_In32(0x80000004);
+		Xil_Out32(0x80000000, NUM8 << 24 | NUM9 << 16 | NUM2 << 8 | NUM3);
+		data = Xil_In32(0x80000000);
+    }
+
+    return 0;
+}
---
+++ axi_lite/axi_2_gpio/README.md
@@ -0,0 +1,29 @@
+# AXI to uart IP
+This file contains a IP for an axi slave to uart.<br>
+<br>
+***MARK -> 99%***
+Tested with TB and a zynq with vitis
+
+´´´
+        REGISTER MAP
+-------------------------------
+|    OFSET     | Encabezado 2 |
+|--------------|--------------|
+| 0x00000      | GPIO OUT     |
+| 0x00004      | GPIO IN      |
+-------------------------------
+
+            GPIO OUT
+-------------------------------
+| (N to 0)     | GPIO TO BE ON|
+-------------------------------
+
+            GPIO IN
+-------------------------------
+| (N to 0)     |GPIO TO BE OFF|
+-------------------------------
+
+´´´
+
+
+<br>
